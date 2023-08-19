ca5cf4864c90e4ffae4fde4d959664a400b22887
get4p <get4push@outlook.com>
2023-08-19 14:00:01 +0200

    Refactor reward pooling algorithm

--- voting_mechanism.rs
+++ voting_mechanism.rs
@@ -2,3 +2,4 @@
 // Integrate with decentralized prediction markets
 // Upgrade to latest Solidity version
 // Implement time-locked contract execution
+// Add support for zk-SNARKs in smart contracts
