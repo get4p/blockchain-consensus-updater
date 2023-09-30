7f92667c9313adc17fcc5e8d63ea3238acf0bd59
get4p <get4push@outlook.com>
2023-09-30 14:00:00 +0200

    Enhance developer documentation

--- liquidity_mining.go
+++ liquidity_mining.go
@@ -1,3 +1,4 @@
 // Optimize gas usage in smart contracts
 // Fix security vulnerability in random number generation
 // Implement price feed oracles for tokens
+// Fix security vulnerability in random number generation
