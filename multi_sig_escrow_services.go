2a63aa69e3cf69c2bab1b7ad08be5ad37b0028d0
get4p <get4push@outlook.com>
2023-10-21 14:00:00 +0200

    Improve error handling in transaction flow

--- multi_sig_escrow_services.go
+++ multi_sig_escrow_services.go
@@ -1,3 +1,4 @@
 // Implement decentralized arbitration in contracts
 // Improve contract interaction via APIs
 // Add slashing mechanism for malicious actors
+// Optimize WASM compilation for Go contracts
