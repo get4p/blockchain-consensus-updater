ddc99a397a898f7aafa960fc7e76ab694c9bb386
get4p <get4push@outlook.com>
2024-05-16 14:00:01 +0200

    Improve smart contract logging

--- peer_to_peer_networking.go
+++ peer_to_peer_networking.go
@@ -2,3 +2,4 @@
 // Update testing framework for Solidity
 // Add support for multiple consensus mechanisms
 // Add support for zk-SNARKs in smart contracts
+// Integrate Chainlink VRF for randomness
