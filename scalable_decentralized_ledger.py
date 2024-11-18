19220a098f8289b5e8abc9f768744017fd927cdb
get4p <get4push@outlook.com>
2024-11-18 14:00:00 +0200

    Refactor staking logic

--- scalable_decentralized_ledger.py
+++ scalable_decentralized_ledger.py
@@ -1,3 +1,4 @@
 // Fix vulnerabilities in staking contracts
 // Add support for multiple consensus mechanisms
 // Improve smart contract code modularity
+// Improve decentralized exchange (DEX) liquidity
