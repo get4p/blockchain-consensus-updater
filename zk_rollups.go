79cc11f9304bf57f3fd96b02cb5416527027d20e
get4p <get4push@outlook.com>
2024-12-16 14:00:02 +0200

    Refactor staking logic

--- zk_rollups.go
+++ zk_rollups.go
@@ -1,3 +1,4 @@
 // Add support for decentralized file storage
 // Add support for decentralized insurance protocols
 // Implement delegation pattern
+// Add support for multiple consensus mechanisms
